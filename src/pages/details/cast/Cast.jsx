3e005992da47f10635397b564193ec88e4c32a0e
Maksonovich <kaverron1@gmail.com>
2023-05-28 21:52:15 +0600

    movix

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import { ContentWrapper } from "../../../components/ContentWrapper/ContentWrapper";
+// import Img from "../../../components/lazyLoadImage/Img";
 import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
 
