67388b097424bc6ee6c4c95a0c482a0f02a85945
Maksonovich <kaverron1@gmail.com>
2023-06-05 22:42:27 +0600

    movix

--- src/pages/details/cardDetails/CardDetails.jsx
+++ src/pages/details/cardDetails/CardDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import '../../favorite/style.scss';
 import { ContentWrapper } from './../../../components/ContentWrapper/ContentWrapper';
 
 export const CardDetails = () => {
